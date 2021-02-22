21e8b0270bff0a4b28f90250bc1de236c37a9436
pontailler.jb@gmail.com <pontailler.jb@gmail.com>
2021-02-22 16:48:29 +0100

    add foreach

--- Projet_1 application_console/Program.cs
+++ Projet_1 application_console/Program.cs
@@ -9,7 +9,7 @@ namespace Projet1
         {
             Dictionary<string, string> questionTrue = new Dictionary<string, string>();
 
-            questionTrue.Add("Question 1", "Réponse");
+            questionTrue.Add("1", "a");
             questionTrue.Add("Question 2", "Réponse");
             questionTrue.Add("Question 3", "Réponse");
             questionTrue.Add("Question 4", "Réponse");
@@ -22,48 +22,74 @@ namespace Projet1
 
             Dictionary<string, string> questionFalse = new Dictionary<string, string>();
 
-            questionFalse.Add("Question 1(F1)", "Réponse Fausse 1 ");
-            questionFalse.Add("Question 1(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 1(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 1(F1)", "a");
+            questionFalse.Add("Question 1(F2)", "b");
+            questionFalse.Add("Question 1(F3)", "c");
 
-            questionFalse.Add("Question 2(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 2(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 2(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 2(F1)", "a");
+            questionFalse.Add("Question 2(F2)", "b");
+            questionFalse.Add("Question 2(F3)", "c");
 
-            questionFalse.Add("Question 3(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 3(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 3(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 3(F1)", "a");
+            questionFalse.Add("Question 3(F2)", "b");
+            questionFalse.Add("Question 3(F3)", "c");
 
-            questionFalse.Add("Question 4(F1)", "Réponse Fausse 1 ");
-            questionFalse.Add("Question 4(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 4(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 4(F1)", "a");
+            questionFalse.Add("Question 4(F2)", "b");
+            questionFalse.Add("Question 4(F3)", "c");
 
-            questionFalse.Add("Question 5(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 5(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 5(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 5(F1)", "a");
+            questionFalse.Add("Question 5(F2)", "b");
+            questionFalse.Add("Question 5(F3)", "c");
 
-            questionFalse.Add("Question 6(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 6(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 6(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 6(F1)", "a");
+            questionFalse.Add("Question 6(F2)", "b");
+            questionFalse.Add("Question 6(F3)", "c");
 
-            questionFalse.Add("Question 7(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 7(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 7(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 7(F1)", "a");
+            questionFalse.Add("Question 7(F2)", "b");
+            questionFalse.Add("Question 7(F3)", "c");
 
-            questionFalse.Add("Question 8(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 8(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 8(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 8(F1)", "a");
+            questionFalse.Add("Question 8(F2)", "b");
+            questionFalse.Add("Question 8(F3)", "c");
 
-            questionFalse.Add("Question 9(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 9(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 9(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 9(F1)", "a");
+            questionFalse.Add("Question 9(F2)", "b");
+            questionFalse.Add("Question 9(F3)", "c");
 
-            questionFalse.Add("Question 10(F1)", "Réponse Fausse 1");
-            questionFalse.Add("Question 10(F2)", "Réponse Fausse 2");
-            questionFalse.Add("Question 10(F3)", "Réponse Fausse 3");
+            questionFalse.Add("Question 10(F1)", "a");
+            questionFalse.Add("Question 10(F2)", "b");
+            questionFalse.Add("Question 10(F3)", "c");
 
-            Console.WriteLine("Question 1" + questionTrue["Question 1"]);
-            string data = Console.ReadLine();
+
+            string answer;
+
+
+
+            foreach (KeyValuePair<string, string> langage in questionTrue)
+            {
+
+                Console.WriteLine("Question 1");
+                answer = Console.ReadLine();
+
+
+                /*Console.WriteLine("Clé: {0}, Valeur: {1}",
+                    langage.Key, langage.Value);*/
+
+                if (answer == questionTrue)
+                {
+                    Console.WriteLine("Coucou");
+                }
+
+                else
+                {
+                    Console.WriteLine("test");
+                }
+                
+            
+                        
+            }
         }
     }
 }
