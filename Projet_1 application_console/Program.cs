d0395c3566d9d7387c218cb61e7ee42a658b902d
Akma <pontailler.jb@gmail.com>
2021-02-22 20:29:40 +0100

    Suppresion dicitonnaire, ajout de liste, début de boucle

--- Projet_1 application_console/Program.cs
+++ Projet_1 application_console/Program.cs
@@ -7,89 +7,46 @@ namespace Projet1
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> questionTrue = new Dictionary<string, string>();
+            List<string[]>questionAnswer = new List<string[]>();
+        
 
-            questionTrue.Add("1", "a");
-            questionTrue.Add("Question 2", "Réponse");
-            questionTrue.Add("Question 3", "Réponse");
-            questionTrue.Add("Question 4", "Réponse");
-            questionTrue.Add("Question 5", "Réponse");
-            questionTrue.Add("Question 6", "Réponse");
-            questionTrue.Add("Question 7", "Réponse");
-            questionTrue.Add("Question 8", "Réponse");
-            questionTrue.Add("Question 9", "Réponse");
-            questionTrue.Add("Question 10", "Réponse");
+        //Liste des questions et réponses, la bonne réponse est toujours la réponse 4
+        string[] items1 = {"Question 1"};
+        string[] items2 = {"Réponse 1"};
+        string[] items3 = {"Réponse 2"};
+        string[] items4 = {"Réponse 3"};
+        string[] items5 = {"Vrai"};
 
-            Dictionary<string, string> questionFalse = new Dictionary<string, string>();
+        string[] items6 = { "Question 2" };
+        string[] items7 = { "Réponse 1" };
+        string[] items8 = { "Réponse 2" };
+        string[] items9 = { "Réponse 3" };
+        string[] items10 = { "vrai" };
 
-            questionFalse.Add("Question 1(F1)", "a");
-            questionFalse.Add("Question 1(F2)", "b");
-            questionFalse.Add("Question 1(F3)", "c");
-
-            questionFalse.Add("Question 2(F1)", "a");
-            questionFalse.Add("Question 2(F2)", "b");
-            questionFalse.Add("Question 2(F3)", "c");
-
-            questionFalse.Add("Question 3(F1)", "a");
-            questionFalse.Add("Question 3(F2)", "b");
-            questionFalse.Add("Question 3(F3)", "c");
-
-            questionFalse.Add("Question 4(F1)", "a");
-            questionFalse.Add("Question 4(F2)", "b");
-            questionFalse.Add("Question 4(F3)", "c");
-
-            questionFalse.Add("Question 5(F1)", "a");
-            questionFalse.Add("Question 5(F2)", "b");
-            questionFalse.Add("Question 5(F3)", "c");
-
-            questionFalse.Add("Question 6(F1)", "a");
-            questionFalse.Add("Question 6(F2)", "b");
-            questionFalse.Add("Question 6(F3)", "c");
-
-            questionFalse.Add("Question 7(F1)", "a");
-            questionFalse.Add("Question 7(F2)", "b");
-            questionFalse.Add("Question 7(F3)", "c");
-
-            questionFalse.Add("Question 8(F1)", "a");
-            questionFalse.Add("Question 8(F2)", "b");
-            questionFalse.Add("Question 8(F3)", "c");
-
-            questionFalse.Add("Question 9(F1)", "a");
-            questionFalse.Add("Question 9(F2)", "b");
-            questionFalse.Add("Question 9(F3)", "c");
-
-            questionFalse.Add("Question 10(F1)", "a");
-            questionFalse.Add("Question 10(F2)", "b");
-            questionFalse.Add("Question 10(F3)", "c");
-
-
-            string answer;
-
-
-
-            foreach (KeyValuePair<string, string> langage in questionTrue)
+            //Pose les 10 questions, et calcul le score sur 10
+            int i = 0;
+            while(i <2) 
             {
 
-                Console.WriteLine("Question 1");
-                answer = Console.ReadLine();
 
+                Console.WriteLine("Test en C#");
+                Console.WriteLine(items1[0]);
+                string answer = Console.ReadLine();
+                    if (answer == "vrai")
+                    {
+                        int result = +1;
+                        i++;
+                        //Affiche le résultat et i
+                        Console.WriteLine("result = " + result + ", i = " + i);
+                    }
 
-                /*Console.WriteLine("Clé: {0}, Valeur: {1}",
-                    langage.Key, langage.Value);*/
-
-                if (answer == questionTrue)
-                {
-                    Console.WriteLine("Coucou");
-                }
-
-                else
-                {
-                    Console.WriteLine("test");
-                }
-                
-            
-                        
+                    else
+                    {
+                        //Anonce la fin du test et donne le résultat
+                        Console.WriteLine("Fin du test, votre résultat est de /10");
+                    }
             }
         }
+        }
     }
-}
+
