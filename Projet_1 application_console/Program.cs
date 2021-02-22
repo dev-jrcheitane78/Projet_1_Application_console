2c21ad2c876ee9c5c3474927ac93c58e3b0873e2
dev-jrcheitane <66439164+dev-jrcheitane@users.noreply.github.com>
2021-02-22 15:36:20 +0100

    modif du Tuple

--- Projet_1 application_console/Program.cs
+++ Projet_1 application_console/Program.cs
@@ -9,25 +9,36 @@ namespace Projet_1_application_console
         static void Main()
 
         {
-           /* Dictionary<string, string> QuestionsReponse = new Dictionary<string, string>();
-            QuestionsReponse.Add("Le mot string est un : " , "chaine de caractere");
-            QuestionsReponse.Add("Le mot integer est un : ", "nombre");
-            QuestionsReponse.Add("Le mot float est un : "  , "nombre a virgule");
-            QuestionsReponse.Add("Le mot bool est un : "   , "Boolean");
-            QuestionsReponse.Add("Le mot char est un : "   , "charactère");*/
+            
 
-            List<Tuple< (int, string), (string, bool), (string,bool), (string, bool), (string, bool)>> ResponseFalse = new List<Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>> ();
-            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>((1, "Qu'est ce qu'une variable"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
-            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>((1, "Qu'est ce qu'une constante"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            //List<Tuple< (int, string), (string[]), (string,bool), (string, bool), (string, bool)>> ResponseFalse = new List<Tuple<(int, string), (new string[]{"bravo", true }), (string, bool), (string, bool), (string, bool)>> ();
+            Tuple<object[], object[], object[], object[], object[]>[] AnswersResponse = {
+                Tuple.Create(new object[] {1, "question 1:"  }, new object[] {"question 2", true },new object[] {"question 3", false },new object[] {"question 4", false },new object[] {"question 5", false })
+            };
+            /*ResponseFalse.(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une variable"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une constante"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une fonction"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une classes"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une methode"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une constante"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une boucle"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une condition"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'un objet"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));
+            ResponseFalse.Add(new Tuple<(int, string), (string, bool), (string, bool), (string, bool), (string, bool)>(( 1, "Qu'est ce qu'une emuneration"), ("bravo", true), ("refalse", false), ("rerefalse", false), ("rerererefalse", false)));*/
 
             void affichageTable ()
             {
-                
+
+                foreach (var item in AnswersResponse)
+                {
+                    Console.WriteLine($"{item.Item2[1].GetType()}" );
+                    Console.WriteLine($"{item.Item2[0]}");
+                }
 
                 Console.BackgroundColor = ConsoleColor.DarkGray;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine(" _____________________________________________________ ");
-                Console.WriteLine($"| {ResponseFalse[1]}                         |");
+                Console.WriteLine($"| {AnswersResponse}                         |");
                 Console.WriteLine("|-----------------------------------------------------|");
                 Console.WriteLine("|  A. Je sais pas                 B. Ca marche bien   |");
                 Console.WriteLine("|                                                     |");
