732e60921ea2641f04c4a447d4a6c3496de0c698
dev-jrcheitane <66439164+dev-jrcheitane@users.noreply.github.com>
2021-02-22 09:36:24 +0100

    creation du projet appliacation console

---
+++ Projet_1 application_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_1_application_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
